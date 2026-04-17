29e33814952b20e47a07f4a4dd4b090f5413a41c
mrityunjay45108 <kumarmrityunjay5210@gmail.com>
2026-04-18 01:22:05 +0530

    userschema

--- app/model/User.ts
+++ app/model/User.ts
@@ -1,1 +1,43 @@
-import mongoose, {Schema,model,models,} from "mongoose";
+import mongoose, { Schema, model, models } from "mongoose";
+import bcrypt from "bcryptjs";
+
+export interface User {
+  email: string;
+  password: string;
+  name: string;
+  _id?: mongoose.Types.ObjectId;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const userSchema = new Schema<User>(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+userSchema.pre("save", async function () {
+
+  if (!this.isModified("password")) {
+    return;
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+  
+    this.password = await bcrypt.hash(this.password, salt);
+  } catch (err: any) {
+    
+    throw err;
+  }
+});
+
+const UserModel = models.User || model<User>("User", userSchema);
+
+export default UserModel;
